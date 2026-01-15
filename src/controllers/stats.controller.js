6ecf29935258cde74803eeaefbc4f97dbe078ac1
Hemantcodes <hy564636@gmail.com>
2026-01-15 23:39:27 +0530

    added set header

--- src/controllers/stats.controller.js
+++ src/controllers/stats.controller.js
@@ -2,6 +2,7 @@ import { DefaultSvg } from "../renderers/svg.renderer.js";
 
 const createStats=async(req,res)=>{
     const {username}=req.params
+    res.setheader("Content-Type","image/svg+xml")
     res.send(DefaultSvg())
 }
 
