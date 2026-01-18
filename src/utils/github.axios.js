3e8349d8718727f2184c931c3d8383f5f8b3ea44
Hemantcodes <hy564636@gmail.com>
2026-01-18 23:46:11 +0530

    added header in github instance

--- src/utils/github.axios.js
+++ src/utils/github.axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 github_axios=axios.create({
-    baseURL:"https://api.github.com"
+    baseURL:"https://api.github.com",
+    headers: {
+    Accept: "application/vnd.github+json",
+  },
 })
 
 export default github_axios
