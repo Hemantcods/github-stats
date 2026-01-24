be86ae021befa54d368806e7b931359d48e62ef5
Hemantcodes <hy564636@gmail.com>
2026-01-24 09:50:10 +0530

    added a create percentage of languages used function

--- src/controllers/stats.controller.js
+++ src/controllers/stats.controller.js
@@ -1,9 +1,18 @@
+
 import { DefaultSvg } from "../renderers/svg.renderer.js";
+import { getRepos,languageData, createPercentage } from "../services/language.services.js";
 
 const createStats=async(req,res)=>{
     const {username}=req.params
-    res.setHeader("Content-Type","image/svg+xml")
-    res.send(DefaultSvg())
+    // res.setHeader("Content-Type","image/svg+xml")
+    // res.send(DefaultSvg())
+    const response=await createPercentage(username)
+    console.log(response)
+    return res.send(response)
+    // console.log(process.env.GITHUB_TOKEN)
+    // const out=await getAllLanguageData(username,repos)
+    // res.json(out)
+    
 }
 
 export {createStats};
